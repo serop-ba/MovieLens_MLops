6fe78eb5bc0629a9c0c1c84fe5bf59228238fe85
seropb <serop@kenkou.de>
2022-04-29 18:33:21 +0200

    configured repo structure according to cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. MovieLens_Mlops documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+MovieLens_Mlops documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning pipeline that applies best practices of MLops on recommendation task using the benchmark dataset MovieLens from Kaggle. This project serves purely to demonstrate my MLops skills for maintaining and developing entire machine learning pipeline from data acquisition to model monitoring from scratch',
+    author='serop Baghdadlian',
+    license='MIT',
+)
